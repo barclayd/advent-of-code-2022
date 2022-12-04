eb28c26b2c96c90ab5a37c2c5dd240dfffbe7e2d
Daniel Barclay <daniel.barclay@lloydsbanking.com>
2022-12-04 00:34:28 +0000

    feat: day 3 part 2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -13,7 +13,6 @@ const ASCII_LOWER: [char; 26] = [
 ];
 
 fn get_shared_letter_from_codes(codes: &(String, String)) -> char {
-
     let mut shared_letter: Option<char> = None;
 
     for letter_in_code_one in codes.0.chars() {
@@ -27,6 +26,20 @@ fn get_shared_letter_from_codes(codes: &(String, String)) -> char {
     }
 
     return shared_letter.unwrap();
+}
+
+fn get_common_letter_in_string(strings: &Vec<&str>) -> char {
+    let first_code = strings[0];
+    let second_code = strings[1];
+    let third_code = strings[2];
+
+    for letter in first_code.chars() {
+        if second_code.contains(letter) && third_code.contains(letter) {
+            return letter;
+        }
+    }
+
+    panic!("No common letters found!");
 }
 
 fn to_chunks(string: &str, chunk_size: usize) -> (String, String) {
@@ -76,15 +89,29 @@ fn get_sum_of_priorities(file_path: &str) -> i16 {
     return shared_letters.iter().map(|letter| get_priority_for_char(letter)).sum::<i16>();
 }
 
+fn get_sum_of_priorities_by_group(file_path: &str) -> i16 {
+    let file_contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let file_contents_by_line = file_contents.lines().chunks(3);
+
+    let group_codes = file_contents_by_line.into_iter().map(|line| Vec::from_iter(line)).collect::<Vec<Vec<&str>>>();
+
+    let common_letters = group_codes.iter().map(|group_code| get_common_letter_in_string(group_code)).collect::<Vec<char>>();
+
+    return common_letters.iter().map(|letter| get_priority_for_char(letter)).sum::<i16>();
+}
+
 fn main() {
-    get_priority_for_char(&'P');
-    let sum = get_sum_of_priorities("./test.txt");
-    println!("{}", sum);
+    let sum_of_priorities = get_sum_of_priorities("./input.txt");
+    println!("Part 1: {}", sum_of_priorities);
+    let sum_of_priorities_by_group = get_sum_of_priorities_by_group("./input.txt");
+    println!("Part 2: {}", sum_of_priorities_by_group);
 }
 
 #[cfg(test)]
 mod tests {
     use crate::get_sum_of_priorities;
+    use crate::get_sum_of_priorities_by_group;
 
     #[test]
     fn it_returns_expected_result_for_test_file() {
@@ -96,5 +123,16 @@ mod tests {
     fn it_returns_expected_result_for_input_file() {
         let sum_of_priorities = get_sum_of_priorities("./input.txt");
         assert_eq!(sum_of_priorities, 7811);
+    }
+    #[test]
+    fn get_sum_of_priorities_by_group_for_test_file() {
+        let sum_of_priorities = get_sum_of_priorities_by_group("./test.txt");
+        assert_eq!(sum_of_priorities, 70);
+    }
+
+    #[test]
+    fn get_sum_of_priorities_by_group_for_input_file() {
+        let sum_of_priorities = get_sum_of_priorities_by_group("./input.txt");
+        assert_eq!(sum_of_priorities, 2639);
     }
 }
