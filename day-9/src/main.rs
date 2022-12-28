f53fb06053940d8f890f50af0f2e517395c041f4
barclayd <barclaysd@me.com>
2022-12-28 14:02:05 +0000

    chore: part 2

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -24,36 +24,47 @@ impl Direction {
 }
 #[derive(Default)]
 struct Rope {
-    head: Position,
-    tail: Position,
+    segment: Vec<Position>,
     visited: HashSet<Position>,
 }
 
 impl Rope {
     const DIRECTION: [Position; 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
 
+    fn create(length: usize) -> Self {
+        Self {
+            segment: vec![(0, 0); length],
+            visited: HashSet::new(),
+        }
+    }
+
     fn make_move(&mut self, direction: Direction) {
         let rope_direction = Self::DIRECTION[direction as usize];
-        self.head.0 += rope_direction.0;
-        self.head.1 += rope_direction.1;
+        self.segment[0].0 += rope_direction.0;
+        self.segment[0].1 += rope_direction.1;
 
-        let tail_to_head_row_difference = self.head.0 - self.tail.0;
-        let tail_to_head_col_difference = self.head.1 - self.tail.1;
+        for i in 1..self.segment.len() {
+            let tail_to_head_row_difference = self.segment[i - 1].0 - self.segment[i].0;
+            let tail_to_head_col_difference = self.segment[i - 1].1 - self.segment[i].1;
 
-        if tail_to_head_row_difference.abs() == 0 && tail_to_head_col_difference.abs() > 1 {
-            self.tail.1 += tail_to_head_col_difference.signum();
-        } else if tail_to_head_col_difference.abs() == 0 && tail_to_head_row_difference.abs() > 1 {
-            self.tail.0 += tail_to_head_row_difference.signum();
-        } else if tail_to_head_row_difference.abs() > 1 || tail_to_head_col_difference.abs() > 1 {
-            self.tail.0 += tail_to_head_row_difference.signum();
-            self.tail.1 += tail_to_head_col_difference.signum();
+            if tail_to_head_row_difference.abs() == 0 && tail_to_head_col_difference.abs() > 1 {
+                self.segment[i].1 += tail_to_head_col_difference.signum();
+            } else if tail_to_head_col_difference.abs() == 0
+                && tail_to_head_row_difference.abs() > 1
+            {
+                self.segment[i].0 += tail_to_head_row_difference.signum();
+            } else if tail_to_head_row_difference.abs() > 1 || tail_to_head_col_difference.abs() > 1
+            {
+                self.segment[i].0 += tail_to_head_row_difference.signum();
+                self.segment[i].1 += tail_to_head_col_difference.signum();
+            }
+
+            self.visited.insert(self.segment[self.segment.len() - 1]);
         }
-
-        self.visited.insert(self.tail);
     }
 }
 
-fn get_number_of_positions_the_tail_visits(file_path: &str) -> usize {
+fn get_number_of_positions_the_tail_visits(file_path: &str, rope_length: usize) -> usize {
     let file_contents =
         fs::read_to_string(file_path).expect("Should have been able to read the file");
 
@@ -66,7 +77,7 @@ fn get_number_of_positions_the_tail_visits(file_path: &str) -> usize {
         steps.push((direction, distance));
     }
 
-    let mut rope = Rope::default();
+    let mut rope = Rope::create(rope_length);
     for (direction, distance) in steps {
         for _ in 0..distance {
             rope.make_move(direction);
@@ -79,7 +90,7 @@ fn get_number_of_positions_the_tail_visits(file_path: &str) -> usize {
 fn main() {
     println!(
         "number of positions {}",
-        get_number_of_positions_the_tail_visits("test.txt")
+        get_number_of_positions_the_tail_visits("test.txt", 2)
     );
 }
 
@@ -88,14 +99,26 @@ mod tests {
     use crate::get_number_of_positions_the_tail_visits;
 
     #[test]
-    fn it_returns_expected_number_of_positions_for_test_file() {
-        let count = get_number_of_positions_the_tail_visits("./test.txt");
-        assert_eq!(count, 13);
+    fn it_returns_expected_number_of_positions_for_length_of_2_with_test_file() {
+        let number_of_positions = get_number_of_positions_the_tail_visits("./test.txt", 2);
+        assert_eq!(number_of_positions, 13);
+    }
+
+    #[test]
+    fn it_returns_expected_number_of_positions_for_length_of_10_with_test_file_2() {
+        let number_of_positions = get_number_of_positions_the_tail_visits("./test-2.txt", 10);
+        assert_eq!(number_of_positions, 36);
     }
 
     #[test]
     fn it_returns_expected_number_of_positions_for_input_file() {
-        let count = get_number_of_positions_the_tail_visits("./input.txt");
-        assert_eq!(count, 0);
+        let number_of_positions = get_number_of_positions_the_tail_visits("./input.txt", 2);
+        assert_eq!(number_of_positions, 6464);
+    }
+
+    #[test]
+    fn it_returns_expected_number_of_positions_for_length_of_10_with_input_file() {
+        let number_of_positions = get_number_of_positions_the_tail_visits("./input.txt", 10);
+        assert_eq!(number_of_positions, 2604);
     }
 }
